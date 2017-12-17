5680fc03a86ff799cbdf9a8b45aea0acca694e17
slevano1 <stevenlevano1@gmail.com>
2017-12-17 00:51:07 -0800

    search function works! other stuff still needs work

--- assets/ecomm.js
+++ assets/ecomm.js
@@ -8,9 +8,9 @@
     url: 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=bnyxt2x6nya6s3h7fvt52d26&query='+ textEntered
     }).done(function(data) {
     console.log('data', data);
-    $('search-query').empty();
+    $('#search').empty();
     for (i = 0; i < data.items.length; i++) {
-        $('search-query').append('<div>Name: ' + data.items[i].name + '</div>');
+        $('#search').append('<div>Name: ' + data.items[i].name + '</div>');
         $('#pics-appear-here').prepend("<img src='"+data.items[i].largeImage+"'>");
     }
 
