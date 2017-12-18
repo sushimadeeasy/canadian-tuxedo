1437ebee8700ecabaa0ae7d1b398bcd00f9e1207
slevano1 <stevenlevano1@gmail.com>
2017-12-17 23:57:51 -0800

    trying to pull in price and description

--- assets/ecomm.js
+++ assets/ecomm.js
@@ -1,3 +1,30 @@
+$('#submit').on('click', function(){
+
+    //$('.container-fluid').empty();
+
+    var textEntered = $('#enterText').val();
+
+    $.ajax({
+    url: 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=bnyxt2x6nya6s3h7fvt52d26&query='+ textEntered
+    }).done(function(data) {
+    console.log('data', data);
+    $('.content').empty();
+    for (i = 0; i < data.items.length; i++) {
+        $('.content').prepend('<div>Name: ' + data.items[i].name + '</div>');
+        $('#pics-appear-here').prepend("<img src='"+data.items[i].largeImage+"'>");
+        $('#price').prepend('<div>price: ' +data.items[i].salePrice+'</div>')
+    }
+
+});
+
+    return false;
+
+})
+
+
+
+
+/*
  $('#submit').on('click', function(){
 
     $('#container').empty();
@@ -16,7 +43,27 @@
 
 })
 
+    return false;*/
+/*
+})
+$('#specialButton').on('click', function(){
+
+    $('#container').empty();
+
+    var textEntered = $('#specialText').val();
+
+    $.ajax({
+    url: 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/paginated/items?category=5438&specialOffer=rollback&apiKey=bnyxt2x6nya6s3h7fvt52d26&format=json'
+    }).done(function(data) {
+    console.log('data', data);
+    $('#content1').empty();
+    for (i = 0; i < data.items.length; i++) {
+        // $('.content').append('<div>Name: ' + data.items[i].name + '</div>');
+        $('#pics1').prepend("<img src='"+data.items[i].largeImage+"'>");
+    }
+
+});*/
+
     return false;
 
 })
-
