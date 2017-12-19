f92178ae0c6438fc58a1e3ca67be753fece2080d
slevano1 <stevenlevano1@gmail.com>
2017-12-19 13:41:53 -0800

    got prices in

--- assets/css/ecomm.js
+++ assets/css/ecomm.js
@@ -1,22 +1,34 @@
- $('#submit').on('click', function(){
+ $('#submitText').on('click', function(){
 
-    $('#container').empty();
+   // $('#container').empty();
 
-    var textEntered = $('#enterText').val();
+   var textEntered = $('#enterText').val();
 
-    $.ajax({
+   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=bnyxt2x6nya6s3h7fvt52d26&query='+ textEntered
     }).done(function(data) {
     console.log('data', data);
-    $('search-query').empty();
+    $('.content').empty();
     for (i = 0; i < data.items.length; i++) {
-        $('search-query').append('<div>Name: ' + data.items[i].name + '</div>');
-        $('#pics-appear-here').prepend("<img src='"+data.items[i].largeImage+"'>");
+        // $('.content').prepend('<div>Name: ' + data.items[i].name + '</div>');
+        // $('#pics').prepend("<img src='"+data.items[i].largeImage+"'>");
+        // $('#price').prepend('<div>price: ' +data.items[i].salePrice+'</div>')
+        var picDiv = $("<div>");
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Price: $" + data.items[i].salePrice);
+            // Creating and storing an image tag
+            var itemImg = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            itemImg.attr("src", data.items[i].largeImage);
+            // Appending the paragraph and image tag to the animalDiv
+            $('#pics').prepend(p);
+            $('#pics').prepend(itemImg);
+            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            $("#giphHere").append(picDiv);
     }
 
 })
 
-    return false;
+   return false;
 
 })
-
