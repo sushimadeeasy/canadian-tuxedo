8d26b0874ad837244bff3ea7c245f8cbd3c49500
slevano1 <stevenlevano1@gmail.com>
2017-12-20 14:56:58 -0800

    updated

--- assets/css/ecomm.js
+++ assets/css/ecomm.js
@@ -1,8 +1,8 @@
- $('#submitText').on('click', function(){
+$('#submitText').on('click', function(){
 
    // $('#container').empty();
 
-   var textEntered = $('#enterText').val();
+var textEntered = $('#enterText').val();
 
    $.ajax({
     url: 'https://cryptic-headland-94862.herokuapp.com/http://api.walmartlabs.com/v1/search?apiKey=bnyxt2x6nya6s3h7fvt52d26&query='+ textEntered
@@ -10,21 +10,30 @@
     console.log('data', data);
     $('.content').empty();
     for (i = 0; i < data.items.length; i++) {
-        // $('.content').prepend('<div>Name: ' + data.items[i].name + '</div>');
-        // $('#pics').prepend("<img src='"+data.items[i].largeImage+"'>");
-        // $('#price').prepend('<div>price: ' +data.items[i].salePrice+'</div>')
-        var picDiv = $("<div>");
-            // Creating a paragraph tag with the result item's rating
+                
             var p = $("<p>").text("Price: $" + data.items[i].salePrice);
-            // Creating and storing an image tag
+            
+            var description = $("<p class='anything'>").text("" + data.items[i].name);
+
+            var numbReviews = $("<p class='reviews'>").text("" + data.items[i].numReviews);
+
+            var reviewpic = $("<p>").html("<img src="+data.items[i].customerRatingImage+">");
+
             var itemImg = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
+ 
             itemImg.attr("src", data.items[i].largeImage);
             // Appending the paragraph and image tag to the animalDiv
+            //if function incase reviews are unidentified
+            if(data.items[i].numReviews == null){
+                numbReviews = $("<p>").text("No reviews yet");
+                reviewpic = $("<p>").text("Be the first to leave a review");
+            }
+            //Appending the items on our page
             $('#pics').prepend(p);
+            $('#pics').prepend(numbReviews);
+            $('#pics').prepend(reviewpic);
+            $('#pics').prepend(description);
             $('#pics').prepend(itemImg);
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#giphHere").append(picDiv);
     }
 
 })
